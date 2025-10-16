8c0889299a0eb9e503cfc26387b17c3f0a88ce51
blend10 <b.elezi@syn-tech.ch>
2025-10-16 10:29:30 +0200

    lazy implementation delete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,48 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/general/Header";
 import Footer from "./components/general/Footer";
 import { PRICING_TYPE } from "./components/car/Constans";
 import "./App.css";
+
+// Direct imports (no lazy loading)
+import Home from "./pages/Home";
+import About from "./pages/About";
+import CarInfo from "./pages/CarInfo";
+import FilteredCarPage from "./pages/FilteredCarPage";
+import ReserveCar from "./pages/ReserveCar";
+import Faq from "./pages/Faq";
+import Kontakt from "./pages/Kontakt";
+import Blogs from "./pages/Blogs";
+import BlogsInfo from "./components/blogs/BlogsInfo";
 import FlexRent from "./pages/FlexRent";
 import Customer from "./pages/Customer";
-
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const CarInfo = lazy(() => import("./pages/CarInfo"));
-const FilteredCarPage = lazy(() => import("./pages/FilteredCarPage"));
-const ReserveCar = lazy(() => import("./pages/ReserveCar"));
-const Faq = lazy(() => import("./pages/Faq"));
-const Kontakt = lazy(() => import("./pages/Kontakt"));
-const Blogs = lazy(() => import("./pages/Blogs"));
-const BlogsInfo = lazy(() => import("./components/blogs/BlogsInfo"));
-
-const Loading = () => (
-  <div className="text-center py-20 text-xl">Loading...</div>
-);
 
 function App() {
   return (
     <BrowserRouter>
-      2
       <Header />
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/uberUns" element={<About />} />
-          <Route path="/api/cars/:id" element={<CarInfo />} />
-          <Route
-            path="/firmenkunden"
-            element={<FilteredCarPage pricingType={PRICING_TYPE.COMPANY} />}
-          />
-          <Route
-            path="/privatkunden"
-            element={<FilteredCarPage pricingType={PRICING_TYPE.NORMAL} />}
-          />
-          <Route path="/reserve-car" element={<ReserveCar />} />
-          <Route path="/reserve-car-2" element={<ReserveCar isFinal />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/kontakt" element={<Kontakt />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blogs/:blogId" element={<BlogsInfo />} />
-          <Route path="/flexRent" element={<FlexRent />} />
-          <Route path="/autoAboPro" element={<Customer />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/uberUns" element={<About />} />
+        <Route path="/api/cars/:id" element={<CarInfo />} />
+        <Route
+          path="/firmenkunden"
+          element={<FilteredCarPage pricingType={PRICING_TYPE.COMPANY} />}
+        />
+        <Route
+          path="/privatkunden"
+          element={<FilteredCarPage pricingType={PRICING_TYPE.NORMAL} />}
+        />
+        <Route path="/reserve-car" element={<ReserveCar />} />
+        <Route path="/reserve-car-2" element={<ReserveCar isFinal />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/kontakt" element={<Kontakt />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:blogId" element={<BlogsInfo />} />
+        <Route path="/flexRent" element={<FlexRent />} />
+        <Route path="/autoAboPro" element={<Customer />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
